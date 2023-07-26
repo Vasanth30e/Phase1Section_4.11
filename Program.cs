14e2db394c451725ef704f505ab463d8b47228fb
unknown <vasanthgamer2000@gmail.com>
2023-07-26 19:16:52 +0530

    Changes has been Committed

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section4._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+            Console.ReadKey();
+        }
+
+        public static void runApp()
+        {
+            Queue Q = new Queue(10);
+
+            Q.insert(10);
+            Q.insert(20);
+            Q.insert(30);
+            Q.insert(40);
+            Q.insert(50);
+            Q.insert(60);
+            Q.insert(70);
+            Q.insert(80);
+            Q.insert(90);
+            Q.insert(100);
+
+            Console.WriteLine(Q.printQueue());
+
+            Q.delete();
+            Q.delete();
+
+            Console.WriteLine(Q.printQueue());
+        }
+
+    }
+}
